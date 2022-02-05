e5511421d22e07bae671c7a5cb9822168b23a99b
Stephen Parson <me@steveparson.ca>
2022-02-05 18:06:47 -0400

    feat: initial launcher

---
+++ launcher/run_with_podman.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+CONTAINER=insta-game-launcher
+POD=${CONTAINER}pod
+podman container stop $CONTAINER
+podman container rm $CONTAINER
+podman pod rm $POD
+podman pod create --name $POD --publish=5000:5000
+podman build --format docker --no-cache -t $CONTAINER:latest .
+podman run -dit --name=$CONTAINER --pod=$POD $CONTAINER:latest
+curl -X POST -F 'game=xonotic' -F 'config_file=https://raw.githubusercontent.com/xonotic/xonotic/master/server/server.cfg' http://127.0.0.1:5000/game
