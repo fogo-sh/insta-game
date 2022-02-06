b785b3513200bdeaa687401a0727b3836f85492a
Stephen Parson <k47swp@mun.ca>
2022-02-05 22:40:07 -0400

    feat: pull ip address from aws

--- launcher/service.py
+++ launcher/service.py
@@ -1,4 +1,5 @@
 import os
+import boto3
 
 from flask import Flask, render_template
 from flask import request
@@ -8,19 +9,73 @@ app = Flask(__name__, template_folder='static')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    try:
+        response = get_game_state()
+    except Exception as e:
+        response = str(e)
+
+    return render_template('index.html', response=response)
+
+
+def kill_existing_game():
+    print('Killing existing game')
+    pass
+
+
+def run_game(game, config_file):
+    print(f'Running {game} with {config_file} config')
+    pass
 
 
 @app.route('/game', methods=['POST', 'DELETE'])
 def game():
-    if request.method == 'POST':
-        data = request.form
-        return f"{data['game']} {data['config_file']}\n"
+    try:
 
-    if request.method == 'DELETE':
-        return "deleted\n"
+        if request.method == 'POST':
+            kill_existing_game()
+            run_game(request.form['game'], request.form['config_file'])
+            return 'initiated'
+
+        if request.method == 'DELETE':
+            kill_existing_game()
+            return 'deleted'
+
+        if request.method == 'GET':
+            return get_game_state()
+
+    except Exception as e:
+        log(str(e))
+        return str(e)
 
 
-if __name__ == "__main__":
+def get_game_state():
+    ecs = boto3.client('ecs', region_name='ca-central-1')
+    ec2 = boto3.client('ec2', region_name='ca-central-1')
+    tasks = ecs.list_tasks(cluster='insta-game-cluster')
+    log(tasks)
+
+    if len(tasks['taskArns']) == 0:
+        return {'status': 'offline'}
+
+    task_arn = tasks['taskArns'][0]
+    log(task_arn)
+
+    task_info = ecs.describe_tasks(cluster='insta-game-cluster', tasks=[task_arn])
+    log(task_info)
+
+    eni = task_info['tasks'][0]['attachments'][0]['details'][1]['value']
+    log(eni)
+
+    neti = ec2.describe_network_interfaces(NetworkInterfaceIds=[eni])
+    log(neti)
+
+    return {'status': 'online', 'public_ip': neti['NetworkInterfaces'][0]['Association']['PublicIp']}
+
+
+def log(msg):
+    print(f'DEBUG: {msg}')
+
+
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
