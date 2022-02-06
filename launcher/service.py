9429be74472b310f6eac61a295a0108af3acfb4f
Stephen Parson <k47swp@mun.ca>
2022-02-06 14:58:32 -0400

    feat: add on/off, config file swapping, default config file

--- launcher/service.py
+++ launcher/service.py
@@ -1,8 +1,14 @@
 import os
-import boto3
+from time import sleep
 
+import boto3
+import requests
 from flask import Flask, render_template
 from flask import request
+
+XONOTIC_CONFIG_FILE = 'xonotic_config_file'
+CLUSTER = 'insta-game-cluster'
+REGION_NAME = 'ca-central-1'
 
 app = Flask(__name__, template_folder='static')
 
@@ -17,28 +23,14 @@ def home():
     return render_template('index.html', response=response)
 
 
-def kill_existing_game():
-    print('Killing existing game')
-    pass
-
-
-def run_game(game, config_file):
-    print(f'Running {game} with {config_file} config')
-    pass
-
-
-@app.route('/game', methods=['POST', 'DELETE'])
+@app.route('/game', methods=['POST', 'DELETE', 'GET'])
 def game():
     try:
-
         if request.method == 'POST':
-            kill_existing_game()
-            run_game(request.form['game'], request.form['config_file'])
-            return 'initiated'
+            return run_game()
 
         if request.method == 'DELETE':
-            kill_existing_game()
-            return 'deleted'
+            return kill_existing_game()
 
         if request.method == 'GET':
             return get_game_state()
@@ -48,10 +40,52 @@ def game():
         return str(e)
 
 
+@app.route('/config', methods=['GET', 'POST'])
+def get_config():
+    if request.method == 'GET':
+        with open(XONOTIC_CONFIG_FILE, 'rb') as f:
+            return f.read(-1)
+
+    if request.method == 'POST':
+        swap_out_config_file(request.form['config'])
+        return ''
+
+
+def run_game():
+    kill_existing_game()
+    change_desired_count(1)
+    return wait_for_game_state('online', 60)
+
+
+def kill_existing_game():
+    change_desired_count(0)
+    return wait_for_game_state('offline', 60)
+
+
+def change_desired_count(desired_count):
+    ecs = boto3.client('ecs', region_name=REGION_NAME)
+    services = ecs.list_services(cluster=CLUSTER)
+    service = services['serviceArns'][0]
+    ecs.update_service(cluster=CLUSTER, service=service, desiredCount=desired_count)
+
+
+def wait_for_game_state(desired_status, max_wait_period):
+    state = get_game_state()
+
+    for i in range(10):
+        if state['status'] == desired_status:
+            return state
+        else:
+            sleep(min(5, max_wait_period // 10))
+            state = get_game_state()
+
+    return state
+
+
 def get_game_state():
-    ecs = boto3.client('ecs', region_name='ca-central-1')
-    ec2 = boto3.client('ec2', region_name='ca-central-1')
-    tasks = ecs.list_tasks(cluster='insta-game-cluster')
+    ecs = boto3.client('ecs', region_name=REGION_NAME)
+    ec2 = boto3.client('ec2', region_name=REGION_NAME)
+    tasks = ecs.list_tasks(cluster=CLUSTER)
     log(tasks)
 
     if len(tasks['taskArns']) == 0:
@@ -60,7 +94,7 @@ def get_game_state():
     task_arn = tasks['taskArns'][0]
     log(task_arn)
 
-    task_info = ecs.describe_tasks(cluster='insta-game-cluster', tasks=[task_arn])
+    task_info = ecs.describe_tasks(cluster=CLUSTER, tasks=[task_arn])
     log(task_info)
 
     eni = task_info['tasks'][0]['attachments'][0]['details'][1]['value']
@@ -70,6 +104,19 @@ def get_game_state():
     log(neti)
 
     return {'status': 'online', 'public_ip': neti['NetworkInterfaces'][0]['Association']['PublicIp']}
+
+
+def trigger_restart():
+    # TODO: tell container to restart game
+
+    pass
+
+
+def swap_out_config_file(config_file):
+    with open(XONOTIC_CONFIG_FILE, 'wb') as f:
+        f.write(requests.get(config_file).content)
+    if get_game_state()['state'] == 'online':
+        trigger_restart()
 
 
 def log(msg):
