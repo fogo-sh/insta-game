a89c76bdebd40377c9b1bab358d136f191791b21
Steve Parson <k47swp@mun.ca>
2022-09-16 01:29:21 -0300

    fix: Set up version agnostic pk3 copy path

--- docker-containers/xonotic/clean.sh
+++ docker-containers/xonotic/clean.sh
@@ -6,8 +6,8 @@ rm -rf ./Xonotic-clean
 mkdir -p ./Xonotic-clean/data
 
 cp ./Xonotic/xonotic-linux64-dedicated ./Xonotic-clean
-cp ./Xonotic/data/xonotic-20170401-data.pk3 ./Xonotic-clean/data
-cp ./Xonotic/data/xonotic-20170401-maps.pk3 ./Xonotic-clean/data
-cp ./Xonotic/data/xonotic-20170401-nexcompat.pk3 ./Xonotic-clean/data
+cp ./Xonotic/data/xonotic-*-data.pk3 ./Xonotic-clean/data
+cp ./Xonotic/data/xonotic-*-maps.pk3 ./Xonotic-clean/data
+cp ./Xonotic/data/xonotic-*-nexcompat.pk3 ./Xonotic-clean/data
 
 echo "Copied data!"
