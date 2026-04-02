e34101d9c09b198f30918784fc3cb3c9117cf177
Steve Parson <me@steveparson.ca>
2026-04-02 13:12:50 -0400

    fix: xonotic arm build step now downloads assets

---
+++ docker-containers/xonotic-arm/download.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+FILE=xonotic.zip
+SHA_512_SUM=cb39879e96f19abb2877588c2d50c5d3e64dd68153bec3dd1bebedf4d765e506afa419c28381d7005aed664cb1a042571c132b5b319e4308cab67745d996c2a6
+
+validate() {
+  echo "$SHA_512_SUM $FILE" | sha512sum -c
+}
+
+exists() {
+  echo "$FILE exists, validating checksum"
+  validate
+}
+
+if [ -f "$FILE" ]; then
+  exists
+  unzip "$FILE"
+else
+  echo "$FILE does not exist, downloading..."
+  curl https://dl.xonotic.org/xonotic-0.8.6.zip -L -o "$FILE"
+  exists
+  unzip "$FILE"
+fi
