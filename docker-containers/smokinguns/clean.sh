10c51443fe51d52647633eb99202dab845dafffe
Steve Parson <me@steveparson.ca>
2026-04-05 17:06:32 -0400

    revert(games): remove smokinguns scaffold

--- docker-containers/smokinguns/clean.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-set -e
-echo "Nothing to clean for Smokin' Guns; assets are supplied at runtime through DATA_URL."
--- docker-containers/smokinguns/download.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-set -e
-echo "Nothing to pre-download for Smokin' Guns; provide the standalone archive at runtime through DATA_URL."
